f502f1a1d366cf529eeb1665661ec0ca348630e9
dbiaggioli <dbiaggioli@PREVNOTE-20>
2026-03-10 17:17:12 -0300

    ControllerTasks, implementação do CRUD na API

---
+++ taskmanager_api_dotnet/Controllers/TasksController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using taskmanager_api_dotnet.Data;
+using taskmanager_api_dotnet.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace taskmanager_api_dotnet.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TasksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Implement CRUD operations for TaskItem here
+        // Endpoints:
+
+        //GET: api/tasks
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskItem>>> GetTasks()
+        {
+            return await _context.Tasks.ToListAsync();
+        }
+
+        //GET: api/tasks/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TaskItem>> GetTask(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            return task;
+        }
+
+        //POST: api/tasks
+        [HttpPost]
+        public async Task<ActionResult<TaskItem>> PostTask(TaskItem task)
+        {
+            _context.Tasks.Add(task);
+            await _context.SaveChangesAsync();
+
+            //201 - Created
+            return CreatedAtAction(nameof(GetTask), new { id = task.Id }, task);
+        }
+
+        //PUT: api/tasks/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTask(int id, TaskItem task)
+        {
+            if (id != task.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(task).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Tasks.Any(equals => equals.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            //204 - No Content
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTask(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+            //204 - No Content
+            return NoContent();
+        }
+    }
+}
