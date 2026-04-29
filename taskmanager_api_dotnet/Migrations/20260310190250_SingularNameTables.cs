eca3a253c73f6f8d99507a9c042be188ad919c55
dbiaggioli <dbiaggioli@PREVNOTE-20>
2026-04-29 12:44:27 -0300

    feat: atualiza migrations

---
+++ taskmanager_api_dotnet/Migrations/20260310190250_SingularNameTables.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace taskmanager_api_dotnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class SingularNameTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
